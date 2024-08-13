1c715628cfa32b1c5d742f678f9d1cb9ee921366
Maria Lukowich <mlukowich27@gmail.com>
2024-08-12 19:33:52 -0600

    adding general lists to add JS to

--- maria-lukowich-brainflix/src/App.jsx
+++ maria-lukowich-brainflix/src/App.jsx
@@ -38,22 +38,33 @@ function App() {
         </section>
         <section class="comments">
           <h4>3 Comments</h4>
-          <div class="comments__new-comment">
+          <form class="comments__new-comment">
             <label>JOIN THE CONVERSATION</label>
             <input></input>
             <textarea></textarea>
             <img></img>
-          </div>
-          <button>COMMENT</button>
+            <button>COMMENT</button>
+          </form>
         </section>
 
           <ul class="comments-list">
-
+            {/* created in JavaScript */}
+            <li class="comments-list__item">
+              <img class="comments-list__profile-img"></img>
+              <h3 class="comments-list__name"></h3>
+              <p class="comments-list__content"></p>
+              <h3 class="comment-list__date"></h3>
+            </li>
           </ul>
 
-        <section class="next-videos">
-
-        </section>
+        <ul class="next-videos">
+          <li class="next-videos__item">
+            <img class="next-videos__img"></img>
+            <h3 class="next-videos__title"></h3>
+            <h3 class="next-videos__creator"></h3>
+          </li>
+        </ul>
+        
       </main>
     </body>
     <footer>
