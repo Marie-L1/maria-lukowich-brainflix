2fef29d7f33628c172da0ffc977a958f736baeb8
Maria Lukowich <mlukowich27@gmail.com>
2024-08-13 20:19:46 -0600

    flex mixins and upload button

--- maria-lukowich-brainflix/src/App.jsx
+++ maria-lukowich-brainflix/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
     <>
       <Header />
         {/* this section will be created by JavaScript */}
-        <section class="video-player">
+        <section className="video-player">
           <video></video>
           <h1>The Future of Artifical Intelligence</h1>
           <p>By</p>
@@ -15,12 +15,12 @@ function App() {
           <p>Views</p>
           <p>likes</p>
         </section>
-        <section class="video-description">
+        <section className="video-description">
           <p></p>
         </section>
-        <section class="comments">
+        <section className="comments">
           <h4>3 Comments</h4>
-          <form class="comments__new-comment">
+          <form className="comments__new-comment">
             <label>JOIN THE CONVERSATION</label>
             <input></input>
             <textarea></textarea>
@@ -29,21 +29,21 @@ function App() {
           </form>
         </section>
 
-          <ul class="comments-list">
+          <ul className="comments-list">
             {/* created in JavaScript */}
-            <li class="comments-list__item">
-              <img class="comments-list__profile-img"></img>
-              <h3 class="comments-list__name"></h3>
-              <p class="comments-list__content"></p>
-              <h3 class="comment-list__date"></h3>
+            <li className="comments-list__item">
+              <img className="comments-list__profile-img"></img>
+              <h3 className="comments-list__name"></h3>
+              <p className="comments-list__content"></p>
+              <h3 className="comment-list__date"></h3>
             </li>
           </ul>
 
-        <ul class="next-videos">
-          <li class="next-videos__item">
-            <img class="next-videos__img"></img>
-            <h3 class="next-videos__title"></h3>
-            <h3 class="next-videos__creator"></h3>
+        <ul className="next-videos">
+          <li className="next-videos__item">
+            <img className="next-videos__img"></img>
+            <h3 className="next-videos__title"></h3>
+            <h3 className="next-videos__creator"></h3>
           </li>
         </ul>
       
--- maria-lukowich-brainflix/src/components/Header/Header.jsx
+++ maria-lukowich-brainflix/src/components/Header/Header.jsx
@@ -1,18 +1,19 @@
 import "./Header.scss";
+import logo from '../../assets/Logo/BrainFlix-logo.svg';
+import profile from '../../assets/Images/Mohan-muruge.jpg';
 
 function Header() {
     return(
         <header>
             <section className="nav-logo">
-              <img className="nav-logo__img" src="../../assets/Logo/BrainFlix-logo.svg" alt="BrainFlix play button"></img>
-              <h2 className="nav-logo__title">BrainFlix</h2>
+              <img className="nav-logo__img" src={logo} alt="BrainFlix play button"></img>
             </section>
             <section className="nav-input">
               <div className="nav-input__search-bar">
                 <img className="nav-input__search-img" src=""></img>
-                <input className="nav-input__search-box" name="search-input" place-holder="Search"></input>
+                <input className="nav-input__search-box" name="search-input" placeholder="Search"></input>
               </div>
-              <img className="nav-input__profile-img" src="../../assets/Images/Mohan-muruge.jpg"></img>
+              <img className="nav-input__profile-img" src={profile}></img>
             </section>
             <section className="upload">
               <button className="upload__btn">UPLOAD</button>
