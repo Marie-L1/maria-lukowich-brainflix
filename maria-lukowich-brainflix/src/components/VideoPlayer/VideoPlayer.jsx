0ec2d845d564fdd96707f97c44bae612fc7ddae3
Maria Lukowich <mlukowich27@gmail.com>
2024-08-15 17:45:36 -0600

    finishing the styling for the header, and videoplayer

--- maria-lukowich-brainflix/src/components/VideoPlayer/VideoPlayer.jsx
+++ maria-lukowich-brainflix/src/components/VideoPlayer/VideoPlayer.jsx
@@ -5,24 +5,25 @@ import likes_img from '../../assets/Icons/likes.svg';
 function VideoPlayer({ video, poster, title, channel, timestamp, views, likes}){
     return (
       <section className="video-player">
-        <video className="video-player__video" controls poster={poster}>
-          <source src={video}></source>
-        </video>
+        <div className="video-player__video-wrapper">
+          <video className="video-player__video" controls poster={poster}>
+            <source src={video}></source>
+          </video>
+        </div>
         <h1 className="video-player__title">{title}</h1>
         <div className="video-player__info">
           <div className="video-player__info--left">
             <p className="video-player__channel">By {channel}</p>
             <p className="video-player__timestamp">{new Date(timestamp).toLocaleDateString()}</p>
           </div>
-
             <div className="video-player__info--right">
               <img className="video-player__views-img" src={views_img} alt="views"></img>
               <p className="video-player__views">{views}</p>
-
+        
               <img className="video-player__likes-img" src={likes_img} alt="likes"></img>
               <p className="video-player__likes">{likes}</p>
             </div>
-        </div>
+          </div>
       </section>
     );
 }
