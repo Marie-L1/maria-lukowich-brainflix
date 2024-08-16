e6c49458842f4cd7658ed97fc350f091db5064ee
Maria Lukowich <mlukowich27@gmail.com>
2024-08-16 15:41:54 -0600

    adjusting video player info

--- maria-lukowich-brainflix/src/components/VideoPlayer/VideoPlayer.jsx
+++ maria-lukowich-brainflix/src/components/VideoPlayer/VideoPlayer.jsx
@@ -14,15 +14,19 @@ function VideoPlayer({ video, poster, title, channel, timestamp, views, likes}){
         <h1 className="video-player__title">{title}</h1>
 
         <div className="video-player__info">
-          <div className="video-player__info--left">
+          <div className="video-player__info-left">
             <p className="video-player__channel">By {channel}</p>
             <p className="video-player__timestamp">{new Date(timestamp).toLocaleDateString()}</p>
           </div>
-            <div className="video-player__info--right">
+          <div className="video-player__info-wrapper" >
+            <div className="video-player__info-views">
                 <img className="video-player__views-img" src={views_img} alt="views"></img>
                 <p className="video-player__views">{views}</p>
+            </div>
+            <div className="video-player__info-likes">
                 <img className="video-player__likes-img" src={likes_img} alt="likes"></img>
                 <p className="video-player__likes">{likes}</p>
+          </div>
           </div>
           </div>
       </section>
