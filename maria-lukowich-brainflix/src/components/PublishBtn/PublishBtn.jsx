1e7f49eb5a3c71ce2ecbc1adaed6bf2e96755376
Maria Lukowich <mlukowich27@gmail.com>
2024-08-22 20:12:41 -0600

    adding an alert for the publish button

--- maria-lukowich-brainflix/src/components/PublishBtn/PublishBtn.jsx
+++ maria-lukowich-brainflix/src/components/PublishBtn/PublishBtn.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import "./PublishBtn.scss";
 
 function PublishBtn() {
+  const handlePublishClick = () =>{
+    alert("Your video has successfully been submitted")
+  }
   return (
     <section className="btns">
-        <button className="btns__publish">PUBLISH</button>
+        <button className="btns__publish" onClick={handlePublishClick}>PUBLISH</button>
         <button className="btns__cancel">CANCEL</button>
     </section>
   )
