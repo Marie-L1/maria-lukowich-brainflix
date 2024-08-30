81656d9fea6c5ec3dc7fa81234d6f4dfc6f4786d
Maria Lukowich <mlukowich27@gmail.com>
2024-08-29 21:20:30 -0600

    setting up the upload page to post videos

--- maria-lukowich-brainflix/src/pages/UploadPage/UploadPage.jsx
+++ maria-lukowich-brainflix/src/pages/UploadPage/UploadPage.jsx
@@ -1,16 +1,45 @@
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 import "./UploadPage.scss"
 import Hero from "../../components/UploadHero/Hero"
 import UploadInput from "../../components/UploadInput/UploadInput"
-import PublishBtn from "../../components/PublishBtn/PublishBtn"
+
+const API_URL = import.meta.env.VITE_API_URL;
 
 function UploadPage(){
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+    const navigate = useNavigate(); // Hook for navigation
+
+   const handleFormSubmit = async (formData) => {
+    try{
+        const response = await axios.post(`${API_URL}/videos/`, {
+            title: formData.title,
+            description: formData.description,
+    });
+    alert("Video successfully uploaded")
+    console.log(response.data)
+
+    // redirect to the homepage
+    navigate("/")
+    }catch(error){
+        console.error("Error uploading video", error)
+    }
+   }
+
     return(
         <>
         <article>
         <Hero />
-        <UploadInput />
+        <UploadInput 
+             title={title}
+             description={description}
+             setTitle={setTitle}
+             setDescription={setDescription}
+             handleFormSubmit={handleFormSubmit}
+             />
         </article>
-        <PublishBtn />
         </>
     )
 }
